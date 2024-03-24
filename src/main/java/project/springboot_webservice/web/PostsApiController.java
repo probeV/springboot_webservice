5618df84249fc802a848f1c5976086f8f3c1e79d
Ahnjaemin1208 <michael1208@naver.com>
2024-03-24 18:51:48 +0900

    Feat : Save, Post, Update
    API

--- src/main/java/project/springboot_webservice/web/PostsApiController.java
+++ src/main/java/project/springboot_webservice/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package project.springboot_webservice.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.springboot_webservice.service.posts.PostsService;
+import project.springboot_webservice.web.dto.PostsResponseDto;
 import project.springboot_webservice.web.dto.PostsSaveRequestDto;
+import project.springboot_webservice.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +16,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
